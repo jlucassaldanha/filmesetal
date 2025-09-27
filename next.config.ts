67df569deaa870d5f2245a0b26df9c1aca2656c1
João Saldanha <jlucassaldanha@gmail.com>
2025-09-26 23:18:55 -0300

    Criação incial pagina home

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/**', // Permite qualquer imagem dentro do caminho /t/p/
+      },
+    ],
+  },
 };
 
 export default nextConfig;
